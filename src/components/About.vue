6744bee6056fcc503df50800021497608e360a4d
zhangcui <zhangcui@lpet.com.cn>
2018-05-04 14:08:19 +0800

    vue practice init

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>About {{ name }}</h1>
+        <Button type="primary">Primary</Button>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+    import { Button } from 'iview';
+    export default {
+        name: 'About',
+        computed: mapState('about',['name']),
+        components: { Button }
+    }
+</script>
